a0fb7bac06e6975fa86d832a56ba4b6d0dbd0f2d
kalmeidalopez <kalmeidalopez@gmail.com>
2023-01-05 14:49:17 -0500

    Add Project Tienda

---
+++ Infrastructure/Data/TiendaContext.cs
@@ -0,0 +1,24 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Data;
+
+public class TiendaContext : DbContext
+{
+    public TiendaContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Marca> Marcas { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+}
